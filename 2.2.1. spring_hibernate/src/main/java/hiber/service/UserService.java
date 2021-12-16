bb921ca27dd1da4d9221c2dc35315ae498620f8d
VitalY <simferopol.jurart@gmail.com>
2021-12-16 17:20:28 +0300

    1 Commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,19 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+
+    @Transactional
+    void add(Car car);
+
+    List<User> listUsers();
+
+    User getUserByCar(String model, int series);
+
+}
